b6e3026b32bfdc7eab761d428e3215c92dc91798
Aobo Yang <yangaobo@gmail.com>
2019-12-13 16:43:54 -0500

    HW6: GMM

--- HW6/clustering.py
+++ HW6/clustering.py
@@ -7,6 +7,7 @@ import collections
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from sklearn.mixture import GaussianMixture
 
 def loadData(fileDj):
     #Your code here
@@ -131,58 +132,99 @@ def purity(clusters, labels):
 ## GMM functions
 
 #calculate the initial covariance matrix
-#covType: diag, full
-def getInitialsGMM(X,k,covType):
-    if covType == 'full':
-        dataArray = np.transpose(np.array([pt[0:-1] for pt in X]))
+#cov_type: diag, full
+def getInitialsGMM(X, k, cov_type):
+    if cov_type == 'full':
+        dataArray = np.transpose(X)
         covMat = np.cov(dataArray)
     else:
-        covMatList = []
-        for i in range(len(X[0])-1):
-            data = [pt[i] for pt in X]
-            cov = np.asscalar(np.cov(data))
-            covMatList.append(cov)
+        covMatList = [
+            np.cov(X[:, i]).item()
+            for i in range(X.shape[1])
+        ]
         covMat = np.diag(covMatList)
 
-    initialClusters = {}
     #Your code here
-    return initialClusters
+    cluster_portions = np.array([1 / k] * k)
+    centroids = getInitialCentroids(X, k)
+    return centroids, covMat, cluster_portions
 
 
-def calcLogLikelihood(X,clusters,k):
-    loglikelihood = 0
-    #Your code here
+def calcLogLikelihood(X_probs):
+    loglikelihood = np.log(X_probs).sum()
     return loglikelihood
 
-#E-step
-def updateEStep(X,clusters,k):
-    EMatrix = []
-    #Your code here
-    return EMatrix
+# E-step
+def updateEStep(X, means, cov_mat, cluster_portions):
+    cov_mat_inv = np.linalg.inv(cov_mat)
 
-#M-step
-def updateMStep(X,clusters,EMatrix):
-    #Your code here
-    return clusters
+    E_matrix = np.array([[
+        np.exp(-0.5 * (point - mu).T @ cov_mat_inv @ (point - mu)) * cp
+        for mu, cp in zip(means, cluster_portions)
+    ] for point in X])
 
-def visualizeClustersGMM(X,labels,clusters,covType):
-    #Your code here
-    pass
+    X_probs = E_matrix.sum(1)
+    E_matrix /= X_probs[:, np.newaxis]
 
-def gmmCluster(X, k, covType, maxIter=1000):
+    return E_matrix, X_probs
+
+# M-step
+def updateMStep(X, E_matrix):
+    #Your code here
+
+    # centroids = np.array([
+    #     (X * E_matrix[:, c:c+1]).sum(0) / E_matrix[:, c].sum()
+    #     for c in range(E_matrix.shape[1])
+    # ])å
+
+    means = E_matrix.T @ X / E_matrix.sum(0)[:, np.newaxis]
+
+    cluster_portions = E_matrix.sum(0) / E_matrix.shape[0]
+
+    return means, cluster_portions
+
+def visualizeClustersGMM(X, labels, cov_type):
+    #Your code here
+    colors = cm.get_cmap('Pastel1').colors
+
+    k = labels.max()
+    for c, color in zip(range(k + 1), colors):
+        c_idxes = (labels == c)
+        plt.scatter(X[c_idxes, 0], X[c_idxes, 1], color=color)
+
+    plt.title('GMM ' + cov_type)
+    plt.show()
+
+def gmmCluster(X, k, cov_type, max_iter=1000):
     #initial clusters
-    clustersGMM = getInitialsGMM(X,k,covType)
-    labels = []
+    clustersGMM = getInitialsGMM(X, k, cov_type)
+    centroids, cov_mat, cluster_portions = clustersGMM
+
+    E_matrix = None
+    ll = -float('inf')
+
     #Your code here
-    visualizeClustersGMM(X,labels,clustersGMM,covType)
-    return labels,clustersGMM
+    for i in range(max_iter):
+        new_E_matrix, X_probs = updateEStep(X, centroids, cov_mat, cluster_portions)
 
+        new_ll = calcLogLikelihood(X_probs)
 
-def purityGMM(X, clusters, labels):
-    purities = []
-    #Your code here
-    return purities
+        # stop when loglikelihood increases less than a threshold
+        if new_ll - ll < 0.00001:
+            break
 
+        E_matrix = new_E_matrix
+        ll = new_ll
+
+        centroids, cluster_portions = updateMStep(X, E_matrix)
+
+        if (i + 1) % 100 == 0:
+            print(f'iteration {i + 1}:', ll)
+
+    labels = E_matrix.argmax(1)
+
+    visualizeClustersGMM(X, labels, cov_type)
+    return labels
 
 
 
@@ -194,32 +236,45 @@ def main():
     dataset1, labels1 = loadData(pathDataset1)
     dataset2, labels2 = loadData(pathDataset2)
 
-    #Q2,Q3
-    point_clusters = kmeans(dataset1, 2, max_iter=1000)
+    # #Q2,Q3
+    # point_clusters = kmeans(dataset1, 2, max_iter=1000)
 
-    visualizeClusters(dataset1, point_clusters)
+    # visualizeClusters(dataset1, point_clusters)
 
-    #Q4
-    kneeFinding(dataset1,range(1,7))
 
-    #Q5
-    purities = purity(point_clusters, labels1)
-    print('k-mean purities are:', purities)
-    exit()
+    # point_clusters = kmeans(dataset2, 2, max_iter=1000)
+    # visualizeClusters(dataset2, point_clusters)
+
+    # model = GaussianMixture(2, covariance_type='tied')
+    # point_clusters = model.fit_predict(dataset1)
+    # visualizeClusters(dataset1, point_clusters)
+
+    # #Q4
+    # kneeFinding(dataset1,range(1,7))
+
+    # #Q5
+    # purities = purity(point_clusters, labels1)
+    # print('k-mean purities:', purities)
 
     #Q7
-    labels11,clustersGMM11 = gmmCluster(dataset1, 2, 'diag')
-    labels12,clustersGMM12 = gmmCluster(dataset1, 2, 'full')
+    preds11 = gmmCluster(dataset1, 2, 'diag')
+    preds12 = gmmCluster(dataset1, 2, 'full')
 
     #Q8
-    labels21,clustersGMM21 = gmmCluster(dataset2, 2, 'diag')
-    labels22,clustersGMM22 = gmmCluster(dataset2, 2, 'full')
+    preds21 = gmmCluster(dataset2, 2, 'diag')
+    preds22 = gmmCluster(dataset2, 2, 'full')
 
     #Q9
-    purities11 = purityGMM(dataset1, clustersGMM11, labels11)
-    purities12 = purityGMM(dataset1, clustersGMM12, labels12)
-    purities21 = purityGMM(dataset2, clustersGMM21, labels21)
-    purities22 = purityGMM(dataset2, clustersGMM22, labels22)
+    purities11 = purity(preds11, labels1)
+    print('diag GMM purities of dataset1:', purities11)
+    purities12 = purity(preds12, labels1)
+    print('full GMM purities of dataset1:', purities12)
+
+    purities21 = purity(preds21, labels2)
+    print('diag GMM purities of dataset2:', purities21)
+
+    purities22 = purity(preds22, labels2)
+    print('full GMM purities of dataset2:', purities22)
 
 if __name__ == "__main__":
     main()
